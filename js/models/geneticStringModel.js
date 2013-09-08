efb2564053eba821c0d04a1ec95b7e6990fc11a3
Igor Zalutsky <igor.zalutsky@gmail.com>
2013-09-08 14:23:27 +0300

    Memoized prefix and suffix extraction

--- js/models/geneticStringModel.js
+++ js/models/geneticStringModel.js
@@ -4,7 +4,17 @@ define(['underscore', 'backbone'],
         var GeneticStringModel = Backbone.Model.extend({
             initialize: function(){
 
-            }
+            },
+
+            prefix: _.memoize(function(size){
+                var sequence = this.get('sequence');
+                return sequence.slice(0, size - 1);
+            }.bind(this)),
+
+            suffix: _.memoize(function(size){
+                var sequence = this.get('sequence');
+                return sequence.slice(sequence.length - 1 - size);
+            }.bind(this))
         });
         return GeneticStringModel;
     });
