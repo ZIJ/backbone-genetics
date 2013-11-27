d56cc5140b5301dbc009d159871910dd21b60360
Igor Zalutsky <igor.zalutsky@gmail.com>
2013-11-27 18:21:01 +0100

    Changes before moving to Heroku

--- static/js/collections/geneticStringCollection.js
+++ static/js/collections/geneticStringCollection.js
@@ -1,6 +1,7 @@
 define(['underscore', 'backbone', 'models/geneticStringModel'],
     function (_, Backbone, GeneticStringModel) {
         'use strict';
+
         var GeneticStringCollection = Backbone.Collection.extend({
             model: GeneticStringModel,
             initialize: function(){
