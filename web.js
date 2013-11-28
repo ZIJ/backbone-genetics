d9fa28e002fdfdaa13d8f79f223795fdc2cbe98d
Igor Zalutsky <igor.zalutsky@gmail.com>
2013-11-28 19:59:50 +0100

    Added GET and POST endpoints

--- web.js
+++ web.js
@@ -1,9 +1,40 @@
 var express = require("express");
 var app = express();
+var port = process.env.PORT || 5000;
+var mongo = require('mongodb');
+var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/mydb';
+var mongoose = require('mongoose');
+mongoose.connect(mongoUri);
+var db = mongoose.connection;
 
+var geneticStringSchema = new mongoose.Schema({
+    name: String,
+    sequence: String
+});
+
+var GeneticString = mongoose.model('GeneticString', geneticStringSchema);
+
+
+app.use(express.bodyParser());
 app.use(express.static('static'));
 
-var port = process.env.PORT || 5000;
+app.post('/string', function(req, res){
+    var string = new GeneticString({
+        name: req.body.name,
+        sequence: req.body.sequence
+    });
+    string.save(function(err, str){
+        res.send(str);
+    });
+});
+
+app.get('/strings', function(req, res){
+    GeneticString.find(function(err, strings){
+        res.send(strings);
+    });
+});
+
+
 app.listen(port, function() {
     console.log("Listening on " + port);
 });
